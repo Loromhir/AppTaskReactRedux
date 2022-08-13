0e0b26164c540f3761a7b38330864e08a57bd10e
Loromhir <mosqueralorenzo@hotmail.com>
2022-08-13 14:06:07 -0300

    trabajando en el redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React , {useState} from 'react';
+import { NavBar } from './components/NavBar'; 
 import { Routes, Route } from 'react-router-dom' ;
 import Books from './components/Books';
-import { NavBar } from './components/NavBar'; 
 import CreateBook from './components/CreateBook';
 import './styles/App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
+
 function App() {
+
+  
+  const [pathname, setPathname]= useState('');
+
+  function notifyPathname(pathname){
+    setPathname({
+      pathname: pathname
+    });
+  }
+
   return (
-      <Routes>
-        <Route path='/' element={<NavBar/>}/>     {/* //este se renderiza, pero Books nop :( */}
+     <React.Fragment>
+     <NavBar notifyPathname={notifyPathname} pathname={pathname}/>  
+     <Routes>  
         <Route path='/' element={<Books/>} />
         <Route path="/create" element={<CreateBook/>} />
         <Route path="/edit/:id" element={<Books/>} />
     </Routes>
-   
+    </React.Fragment>
       
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
-
-export const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+import React ,{Component} from 'react'
+import {Link} from 'react-router-dom'
+class Nav extends Component{
+  constructor(props){
+    super(props)
+  }
+  componentWillMount(){
+    this.props.notifyPathname(window.location.pathname)
+  }
+  render(){
+    return(
+      <div>
+        {
+        this.props.pathname === '/' ? 
+        <Link to='/create'>Add New</Link> : ''
+        }
+      </div>
+    )
+  }
 }
